cf87d475d2a7fcb261d847c48581460667c56fea
MR.NARTERH <merchantdaniel8@gmail.com>
2024-02-10 22:32:22 +0000

    post

--- lib/Screens/postdrug.dart
+++ lib/Screens/postdrug.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
 class postdrug extends StatefulWidget {
   const postdrug({super.key});
 
@@ -11,37 +12,105 @@ class postdrug extends StatefulWidget {
 }
 
 class _postdrugState extends State<postdrug> {
+  TextEditingController _drugNameController = TextEditingController();
+  TextEditingController _dosageController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree
+    _drugNameController.dispose();
+    _dosageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: _image == null
-                ? Text('No image selected.')
-                : Image.file(_image!),
-          ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              FloatingActionButton(
-                onPressed: () => getImage(ImageSource.camera),
-                tooltip: 'Take Photo',
-                child: Icon(Icons.camera),
+        body: Container(
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 58.0, left: 30, right: 30),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
               ),
-              SizedBox(height: 16),
-              FloatingActionButton(
-                onPressed: () => getImage(ImageSource.gallery),
-                tooltip: 'Pick Image from Gallery',
-                child: Icon(Icons.photo_library),
+              child: Center(
+                child: _image == null
+                    ? Text('No image selected.')
+                    : ClipRRect(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // Adjust the value for the desired roundness
+                        child: Image.file(
+                          _image!,
+                          width: 300,
+                          height: 300,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+              ),
+            ),
+          ),
+          SizedBox(height: 23),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(23), color: Colors.black),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    FloatingActionButton(
+                      backgroundColor: Colors.white,
+                      onPressed: () => getImage(ImageSource.camera),
+                      tooltip: 'Take Photo',
+                      child: Icon(Icons.camera),
+                    ),
+                    SizedBox(height: 16),
+                    FloatingActionButton(
+                      backgroundColor: Colors.white,
+                      onPressed: () => getImage(ImageSource.gallery),
+                      tooltip: 'Pick Image from Gallery',
+                      child: Icon(Icons.photo_library),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 28.0, right: 28),
+                child: TextField(
+                  controller: _drugNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Drug Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 28.0, right: 28),
+                child: TextField(
+                  controller: _dosageController,
+                  decoration: InputDecoration(
+                    labelText: 'Dosage',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
               ),
             ],
           ),
-        ],
+        ]),
       ),
-
-    );
+    ));
   }
+
   File? _image;
 
   final picker = ImagePicker();
